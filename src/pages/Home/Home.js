d5566e620db347a678070d3829179d0f5e8079fa
mnajun <najun83@gmail.com>
2023-09-23 00:28:11 -0300

    modificando

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <Header />
       <div className="home">
-        <h1 className="home__title">Tiger MMA</h1>
+        <h1 className="home__title">Tienda MMA</h1>
 
         <Link to="/shop" className="home__cta">
           Consigue lo mejor para guerreros
