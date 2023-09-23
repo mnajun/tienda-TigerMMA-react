a26a71c48ff71873d822a7814abf32147349a9df
mnajun <najun83@gmail.com>
2023-09-23 00:01:50 -0300

    modificacion de letras

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
     <>
       <Header />
       <div className="home">
-        <h1 className="home__title">Tienda MMA</h1>
+        <h1 className="home__title">Tiger MMA</h1>
 
         <Link to="/shop" className="home__cta">
-          Consigue lo mejor, para guerreros
+          CONSIGUE LO MEJOR PARA GUERREROS
         </Link>
 
         <img src={lobo} alt="remera de lobo" className="home__image" />
